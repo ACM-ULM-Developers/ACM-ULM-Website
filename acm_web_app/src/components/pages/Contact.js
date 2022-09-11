a295d7edb3a829bdead3afcfbc10ffc45b8e8d52
prakaala <kaloasmilokakshaya@gmail.com>
2022-09-11 16:48:36 -0500

    created a framework of calendar

--- acm_web_app/src/components/pages/Contact.js
+++ acm_web_app/src/components/pages/Contact.js
@@ -1,14 +1,55 @@
 import React from 'react'
+import {Calendar, dateFnsLocalizer} from "react-big-calendar"
+import format from "date-fns/format"
+import parse from 'date-fns/parse'
+import startOfWeek from 'date-fns/startOfWeek'
+import getDay from "date-fns/getDay"
+import enUs from "date-fns/locale/en-US"
+import "react-big-calendar/lib/css/react-big-calendar.css"
+
+const locales = {
+  "en-US" : enUs
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales
+})
+
+const events = [
+  {
+    title: "ACM Workshop",
+    allDay: true,
+    start: new Date(2022, 8, 16),
+    end: new Date(2022,8,16)
+  },
+  {
+    title: "LoopBack",
+    allDay: true,
+    start: new Date(2022, 9, 16),
+    end: new Date(2022,9,16)
+  },
+  {
+    title: "Changing Interships",
+    allDay: true,
+    start: new Date(2022, 8, 16),
+    end: new Date(2022,8,16)
+  }
+]
 
 
-//changed class based component into functional component
-export default function Contact() {
+
+const Contact = () => {
   return (
     <div>
-    <h2>Any Qusstion about ACM? </h2>
-    <p>Please reach out to us at <a href="https://www.acm.org/">ACM</a>.
-    </p>
+
+      <Calendar localizer={localizer} events ={events} startAccessor = "start" endAccessor="end" style = {{height:500, margin: "50px"}}/>
     </div>
   )
 }
+
+export default Contact
 
