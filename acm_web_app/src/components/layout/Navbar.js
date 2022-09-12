349ee068c54513896f1ea42eb4e88b2aaaf245e4
Prabin Basnet <prabinbasnet09@gmail.com>
2022-09-12 10:11:51 -0500

    Styled navbar and discord community page

--- acm_web_app/src/components/layout/Navbar.js
+++ acm_web_app/src/components/layout/Navbar.js
@@ -4,14 +4,18 @@ import {NavLink} from 'react-router-dom'
 export default function Navbar(){
 
     return(
-        <div>
-            <ul className="header">
-                <li><NavLink to ="/"> Home </NavLink></li>
-                <li><NavLink to="/stuff"> Academics </NavLink></li>
-                <li><NavLink to="/contact"> Current Events </NavLink></li>
-                <li><NavLink to="/contact"> Executive Members </NavLink></li>
-                <li><NavLink to="/discordComm"> ULM Discord Community </NavLink></li>
-                <li><NavLink to="/alumni"> Alumni </NavLink></li>
+        <div className = "navbar">
+            <input id="menu-toggle" type="checkbox" />
+            <label class='menu-button-container' for="menu-toggle">
+                <div class='menu-button'></div>
+            </label>
+            <ul className="navbar--items">
+                <li><NavLink to ="/"> HOME </NavLink></li>
+                <li><NavLink to="/stuff"> ACADEMICS </NavLink></li>
+                <li><NavLink to="/contact"> CURRENT EVENTS </NavLink></li>
+                <li><NavLink to="/contact"> EXECUTIVE MEMBERS </NavLink></li>
+                <li><NavLink to="/discordComm"> ULM DISCORD COMMUNITY </NavLink></li>
+                <li><NavLink to="/alumni"> CS ALUMNI </NavLink></li>
             </ul>
         </div>
     )
